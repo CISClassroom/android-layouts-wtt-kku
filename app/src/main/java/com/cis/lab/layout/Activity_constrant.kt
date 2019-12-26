48844a95c459715d1e9bbe27866d6ebb39676c71
wtt-kku <withan@kkumail.com>
2019-12-27 00:15:27 +0700

    add

---
+++ app/src/main/java/com/cis/lab/layout/Activity_constrant.kt
@@ -0,0 +1,12 @@
+package com.cis.lab.layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_constrant : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constrant)
+    }
+}
---
+++ app/src/main/java/com/cis/lab/layout/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.cis.lab.layout
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button4.setOnClickListener {
+            val i = Intent(this,LinearActivity::class.java)
+            startActivity(i)
+        }
+
+
+        button5.setOnClickListener {
+            val i = Intent(this, RelativeActivity::class.java)
+            startActivity(i)
+
+        }
+
+        button6.setOnClickListener {
+            val i = Intent(this, ConstrantActivity::class.java)
+            startActivity(i)
+        }
+    }
+}
